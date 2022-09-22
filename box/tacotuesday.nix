3451df6a65cfbdfbbf021525a4705fd717c322e0
Stef Dunlap <stef@kindrobot.ca>
2022-09-22 08:47:26 -0400

    Add tacotuesday

--- box/tacotuesday.nix
+++ box/tacotuesday.nix
@@ -8,20 +8,20 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  networking.hostName = "flippy";
+  networking.hostName = "tacotuesday";
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b3512142-b10c-4f68-9a9d-8ae793c6efc8";
+    { device = "/dev/disk/by-uuid/d2aaed2c-35c3-4ea8-89e7-4a68781f0d97";
       fsType = "ext4";
     };
 
   fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/90A5-9465";
+    { device = "/dev/disk/by-uuid/A6F3-7606";
       fsType = "vfat";
     };
 
@@ -32,10 +32,9 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  hardware.bluetooth.enable = true;
-  services.xserver.wacom.enable = true;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
--- flake.nix
+++ flake.nix
@@ -11,6 +11,25 @@
 
   outputs = { self, nixpkgs, darwin, home-manager }: 
   {
+    nixosConfigurations.tacotuesday = nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+      modules = [
+        ({ config, pkgs, ... }: { 
+          nixpkgs.config.allowUnfreePredicate = (pkg: true);
+        })
+        ./conf/workstation.nix
+        ./box/tacotuesday.nix
+        home-manager.nixosModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.kindrobot.imports = [
+            ./home/kindrobot.nix
+	    ./home/kindrobot-linux.nix
+            ./home/email_accounts.nix
+          ];
+        }
+      ];
+    };
     nixosConfigurations.flippy = nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
       modules = [
