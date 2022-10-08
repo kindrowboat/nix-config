f2d9f058e686008d550bc5efaeca582183b71d77
Stef Dunlap <stef@kindrobot.ca>
2022-10-08 14:12:22 -0400

    update org mode settings

--- home/doom.d/config.el
+++ home/doom.d/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Stef Dunlap"
+      user-mail-address "stef@kindrobot.ca")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -41,6 +41,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+(setq org-agenda-files (directory-files-recursively "~/org/" "\\.org$"))
 
 (after! mu4e
 (setq mail-user-agent             'mu4e-user-agent
