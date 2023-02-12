793a72fb81664b546a1770bc805ec1715e4cc945
Stef Dunlap <stef@kindrobot.ca>
2023-02-12 17:37:29 -0500

    Remove scaling from default awesome

--- conf/awesome_workstation.nix
+++ conf/awesome_workstation.nix
@@ -10,12 +10,6 @@
       luadbi-mysql # Database abstraction layer
     ];
   };
-  services.xserver.dpi = 180;
-  environment.variables = {
-    GDK_SCALE = "2";
-    GDK_DPI_SCALE = "0.5";
-    _JAVA_OPTIONS = "-Dsun.java2d.uiScale=2";
-  };
   programs.slock.enable = true;
   environment.systemPackages = with pkgs; [
     dmenu
