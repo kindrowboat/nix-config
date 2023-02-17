bd904f3d88948dc4af07a92adf6d39945504e3f8
Stef Dunlap <stef@kindrobot.ca>
2023-02-16 23:20:03 -0500

    Use intercept-tools to switch capslock and escape

--- conf/workstation.nix
+++ conf/workstation.nix
@@ -36,8 +36,17 @@
   services.xserver = {
     layout = "us";
     xkbVariant = "";
-    xkbOptions = "caps:swapescape";
+  };
 
+  services.interception-tools = {
+    enable = true;
+    plugins = [pkgs.interception-tools-plugins.caps2esc];
+      udevmonConfig = ''
+        - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+          DEVICE:
+            EVENTS:
+              EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
   };
 
   # Enable CUPS to print documents.
@@ -93,6 +102,7 @@
     hunspellDicts.en_US
     hunspellDicts.fr-any
     inkscape
+    interception-tools
     kate
     keepassxc
     killall
