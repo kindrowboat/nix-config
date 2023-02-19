887532970136355a23ad624d2da6904eb0f51b16
Stef Dunlap <stef@kindrobot.ca>
2023-02-19 11:25:02 -0500

    fixup revenge networking

--- box/revenge.nix
+++ box/revenge.nix
@@ -39,9 +39,9 @@
     interfaces.wlo1.useDHCP = lib.mkDefault true;
     interfaces.enp42s0.ipv4.addresses = [ {
       address = "192.168.1.15";
-      prefixLength = 24;
+      prefixLength = 22;
     } ];
-    defaultGateway = "192.168.1.1";
+    defaultGateway = "192.168.0.1";
     nameservers = [ "8.8.8.8" ];
   };
 
