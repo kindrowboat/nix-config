7e0be35339e0a6478b63d6b1f6682bad0dc16523
Stef Dunlap <stef@kindrobot.ca>
2024-10-22 08:28:45 -0400

    catppuccin for wofi

--- home/sway.nix
+++ home/sway.nix
@@ -648,16 +648,6 @@ in
     pulsemixer
     gnome.nautilus
   ];
-  programs.i3status = {
-    enable = true;
-    general = {
-      colors = true;
-      color_good = green;
-      color_degraded = yellow;
-      color_bad = red;
-      interval = 1;
-    };
-  };
 
   programs.alacritty = {
     enable = true;
@@ -742,6 +732,123 @@ in
       # };
     };
   };
+
+  programs.wofi = {
+    enable = true;
+    style = ''
+      * {
+        font-family: 'Inconsolata Nerd Font', monospace;
+        font-size: 16px;
+      }
+
+      /* Window */
+      window {
+        margin: 0px;
+        padding: 10px;
+        border: 0.16em solid ${lavender};
+        border-radius: 0.1em;
+        background-color: ${base};
+        animation: slideIn 0.5s ease-in-out both;
+      }
+
+      /* Slide In */
+      @keyframes slideIn {
+        0% {
+          opacity: 0;
+        }
+
+        100% {
+          opacity: 1;
+        }
+      }
+
+      /* Inner Box */
+      #inner-box {
+        margin: 5px;
+        padding: 10px;
+        border: none;
+        background-color: ${base};
+        animation: fadeIn 0.5s ease-in-out both;
+      }
+
+      /* Fade In */
+      @keyframes fadeIn {
+        0% {
+          opacity: 0;
+        }
+
+        100% {
+          opacity: 1;
+        }
+      }
+
+      /* Outer Box */
+      #outer-box {
+        margin: 5px;
+        padding: 10px;
+        border: none;
+        background-color: ${base};
+      }
+
+      /* Scroll */
+      #scroll {
+        margin: 0px;
+        padding: 10px;
+        border: none;
+        background-color: ${base};
+      }
+
+      /* Input */
+      #input {
+        margin: 5px 20px;
+        padding: 10px;
+        border: none;
+        border-radius: 0.1em;
+        color: ${text};
+        background-color: ${base};
+        animation: fadeIn 0.5s ease-in-out both;
+      }
+
+      #input image {
+          border: none;
+          color: ${red};
+      }
+
+      #input * {
+        outline: 4px solid ${red}!important;
+      }
+
+      /* Text */
+      #text {
+        margin: 5px;
+        border: none;
+        color: ${text};
+        animation: fadeIn 0.5s ease-in-out both;
+      }
+
+      #entry {
+        background-color: ${base};
+      }
+
+      #entry arrow {
+        border: none;
+        color: ${lavender};
+      }
+
+      /* Selected Entry */
+      #entry:selected {
+        border: 0.11em solid ${lavender};
+      }
+
+      #entry:selected #text {
+        color: ${mauve};
+      }
+
+      #entry:drop(active) {
+        background-color: ${lavender}!important;
+      }
+      '';
+    };
 
   home.pointerCursor = {
     gtk.enable = true;
